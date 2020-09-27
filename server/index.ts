d83b301b627a8f75869716b37424a66bb2ff1edc
Luke Autry <lukeautry@gmail.com>
2020-09-27 12:34:41 -0400

    get client page working

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,6 @@
 import http from "http";
+import path from "path";
+import fs from "fs/promises";
 import bodyParser from "body-parser";
 import chalk from "chalk";
 import express from "express";
@@ -6,6 +8,13 @@ import methodOverride from "method-override";
 import "./controllers/widgets-controller";
 import { RegisterRoutes } from "./routes";
 import { log } from "./utils/log";
+
+interface IError {
+  status?: number;
+  fields?: string[];
+  message?: string;
+  name?: string;
+}
 
 export const server = () => {
   const app = express()
@@ -19,16 +28,25 @@ export const server = () => {
         `Origin, X-Requested-With, Content-Type, Accept, Authorization`
       );
       next();
-    });
+    })
+    .set("view engine", "ejs")
+    .use(express.static("public"));
+
+  let assetsJson: { web: { css: string; js: string } } | undefined;
+
+  app.get("/", async (_req, res) => {
+    if (!assetsJson) {
+      assetsJson = JSON.parse(
+        (
+          await fs.readFile(path.join(__dirname, "../tmp/webpack-assets.json"))
+        ).toString()
+      );
+    }
+
+    res.render(path.join(__dirname, "../client/index.ejs"), { assetsJson });
+  });
 
   RegisterRoutes(app);
-
-  interface IError {
-    status?: number;
-    fields?: string[];
-    message?: string;
-    name?: string;
-  }
 
   app.use(
     (
