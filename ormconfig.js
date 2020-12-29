cc5b6c663852fa059943cc5dd42e65806c0e1473
Luke Autry <lukeautry@gmail.com>
2020-12-29 17:24:56 -0500

    make app node runnable

--- ormconfig.js
+++ ormconfig.js
@@ -1,11 +1,29 @@
+const getPathDependentOptions = () => {
+  if (process.env.COMPILED_JS) {
+    return {
+      entities: [`${__dirname}/dist/node/database/entities/**/*.js`],
+      migrations: [`${__dirname}/dist/node/database/migrations/*.js`],
+      migrationsDir: "dist/node/database/migrations",
+    };
+  } else {
+    return {
+      entities: [`${__dirname}/src/node/database/entities/**/*.ts`],
+      migrations: [`${__dirname}/src/node/database/migrations/*.ts`],
+      migrationsDir: "src/node/database/migrations",
+    };
+  }
+};
+
+const { entities, migrations, migrationsDir } = getPathDependentOptions();
+
 const baseOptions = {
   cli: {
-    migrationsDir: `src/node/database/migrations`,
+    migrationsDir,
   },
-  entities: [`${__dirname}/src/node/database/entities/**/*.ts`],
+  entities,
   host: "localhost",
   logging: false,
-  migrations: [`${__dirname}/src/node/database/migrations/*.ts`],
+  migrations,
   password: "admin",
   port: 5432,
   type: "postgres",
--- src/server/start.ts
+++ src/server/start.ts
@@ -1,3 +1,3 @@
-import { server } from ".";
+import { server } from "./index";
 
 server();
