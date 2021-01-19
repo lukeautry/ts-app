9b4a02ad41ace6ed9bf62358f39400ddad2fb5f5
Luke Autry <lukeautry@gmail.com>
2021-01-19 11:16:20 -0500

    comment out live reload

--- src/cli/webpack/webpack.config.dev.ts
+++ src/cli/webpack/webpack.config.dev.ts
@@ -3,7 +3,7 @@ import { getWebpackConfig } from "./get-webpack-config";
 
 // TODO: Use upstream package when fixed
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const LiveReloadPlugin = require("@kooneko/livereload-webpack-plugin");
+// const LiveReloadPlugin = require("@kooneko/livereload-webpack-plugin");
 
 const publicPath = path.join(__dirname, "../../../.public");
 
@@ -16,8 +16,8 @@ export const devWebpackConfig = getWebpackConfig({
   },
   plugins: [
     // this is broken atm, see: https://github.com/statianzo/webpack-livereload-plugin/issues/63
-    new LiveReloadPlugin({
-      quiet: true,
-    }),
+    // new LiveReloadPlugin({
+    //   quiet: true,
+    // }),
   ],
 });
